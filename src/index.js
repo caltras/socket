6f5f3f507acbb6f5053ed807b925e0503d8b30c4
Claudio Traspadini Oliveira <caltras@gmail.com>
2017-04-13 14:50:06 +0000

    Ajuste na biblioteca

--- src/index.js
+++ src/index.js
@@ -1,39 +1,11 @@
-var debug = require("debug")("main");
-const _ = require("lodash");
-var Server = require("./core/server.socket");
+var Message = require("./core/message");
+var BroadCastMessage = require("./core/broadcast.message");
 var Client = require("./core/client.socket");
+var Server = require("./core/server.socket");
 
-
-var server = new Server().start();
-
-var clients = [];
-var groups = ["message::foo","message::others"];
-for(var i=1;i<=10;i++){
-    var c = new Client("teste"+i);
-    c.connect();
-    c.group = _.sample(groups);
-    clients.push(c);    
-}
-
-setTimeout(function(){
-    debug("Created clients");
-    debug(_.map(server.clients,"name"));
-    
-    server.broadcast({message:"Welcome all..."});
-
-
-    setInterval(function(){
-            
-        var group = _.sample(groups);
-        var client1 = _.sample(clients);
-        var idClient2 = "teste"+_.random(1,10);
-        var m = `from: ${client1.name}, to: ${idClient2}`;
-        var m2 = `from: ${client1.name}, group: ${group}`;
-        
-        client1.send(m,idClient2);
-        setTimeout(function(){
-            client1.broadcast(m2,group);
-        },100);
-
-    },5000);
-},500);
+module.exports = {
+    Client: Client,
+    Server: Server,
+    Message: Message,
+    BroadCastMessage : BroadCastMessage
+};
