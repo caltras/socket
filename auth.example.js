1011ebb074f92f62b86253342661a31632a2d534
Claudio Traspadini Oliveira <caltras@gmail.com>
2017-04-27 19:30:30 +0000

    Register

--- auth.example.js
+++ auth.example.js
@@ -7,15 +7,13 @@ var server = new Server();
 
 server.start(5000);
 
+var client = new ClientAuth("client1");
+    
+client.connect(5000);
+
 setTimeout(function(){
-    var client = new ClientAuth("client1");
-    
-    client.connect(5000);
-    
-    setTimeout(function(){
-    	client.send("Teste");
-    	setTimeout(function(){
-    		client.disconnect();
-    	},500);
-    },1000);
-});
+	client.send("Teste");
+	setTimeout(function(){
+		client.disconnect();
+	},500);
+},1000);
--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ var BroadCastMessage = require("./core/broadcast.message");
 var RegisterMessage = require("./core/register.message");
 var Client = require("./core/client.socket");
 var Server = require("./core/server.socket");
+var AuthClient = require("./core/auth.client.socket");
+var AuthServer = require("./core/auth.server.socket");
 
 module.exports = {
     Client: Client,
+    AuthClient: AuthClient,
     Server: Server,
+    AuthServer: AuthServer,
     Message: Message,
     BroadCastMessage : BroadCastMessage,
     RegisterMessage : RegisterMessage
